31dd7c4b415e6baa65c871faacdc5c3f8cd51d4a
leesper <pascal7718@gmail.com>
2017-11-10 09:01:57 +0800

    lexer unit test

--- ql/lexer_test.go
+++ ql/lexer_test.go
@@ -17,30 +17,119 @@ func TestComment(t *testing.T) {
 	}
 }
 
-func TestPunct(t *testing.T) {
-	lexer := NewLexer(`!	$	(	)	...	:	=	@	[	]	{	|	} # ^ &`)
-	var tokens []Token
+func TestLexesPunctuators(t *testing.T) {
+	lexer := NewLexer("! $ ( ) ... : = @ [ ] { | }")
 	tok := lexer.Read()
-	for tok != TokenEOF {
-		tokens = append(tokens, tok)
-		tok = lexer.Read()
+	expected := Token{BANG, "!"}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
 	}
 
-	expectedLen := 13
-	if len(tokens) != expectedLen {
-		t.Errorf("returned len: %d, expected: %d", len(tokens), expectedLen)
+	tok = lexer.Read()
+	expected = Token{DOLLAR, "$"}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
 	}
 
-	expected := Token{SPREAD, "..."}
-	if tokens[4] != expected {
-		t.Errorf("returned: %v, expected: %v", tokens[4], expected)
+	tok = lexer.Read()
+	expected = Token{LPAREN, "("}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
+	}
+
+	tok = lexer.Read()
+	expected = Token{RPAREN, ")"}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
+	}
+
+	tok = lexer.Read()
+	expected = Token{SPREAD, "..."}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
+	}
+
+	tok = lexer.Read()
+	expected = Token{COLON, ":"}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
+	}
+
+	tok = lexer.Read()
+	expected = Token{EQL, "="}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
+	}
+
+	tok = lexer.Read()
+	expected = Token{AT, "@"}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
+	}
+
+	tok = lexer.Read()
+	expected = Token{LBRACK, "["}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
+	}
+
+	tok = lexer.Read()
+	expected = Token{RBRACK, "]"}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
+	}
+
+	tok = lexer.Read()
+	expected = Token{LBRACE, "{"}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
+	}
+
+	tok = lexer.Read()
+	expected = Token{PIPE, "|"}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
+	}
+
+	tok = lexer.Read()
+	expected = Token{RBRACE, "}"}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
 	}
 }
 
-func TestIllegalPunct(t *testing.T) {
-	lexer := NewLexer(`^`)
+func TestInvalidPunctuators(t *testing.T) {
+	lexer := NewLexer("..")
 	tok := lexer.Read()
-	expected := Token{ILLEGAL, "^"}
+	expected := Token{ILLEGAL, ".." + string(rune(EOF))}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
+	}
+
+	lexer = NewLexer("?")
+	tok = lexer.Read()
+	expected = Token{ILLEGAL, "?"}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
+	}
+
+	lexer = NewLexer("\u203B")
+	tok = lexer.Read()
+	expected = Token{ILLEGAL, "\u203B"}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
+	}
+
+	lexer = NewLexer("\u203b")
+	tok = lexer.Read()
+	expected = Token{ILLEGAL, "\u203b"}
+	if tok != expected {
+		t.Errorf("returned: %v, expected: %v", tok, expected)
+	}
+
+	lexer = NewLexer("ф")
+	tok = lexer.Read()
+	expected = Token{ILLEGAL, "ф"}
 	if tok != expected {
 		t.Errorf("returned: %v, expected: %v", tok, expected)
 	}
@@ -457,191 +546,3 @@ func TestInvalidNumbers(t *testing.T) {
 		t.Errorf("returned: %v, expected: %v", tok, expected)
 	}
 }
-
-// func TestLexicalTokens(t *testing.T) {
-// 	lexer := NewLexerWithSource(`a = 2, 47 3.14159 1e50 -6.0221413e23 "Golang\n\r\t" # and this is a comment`)
-// 	var tokens []Token
-// 	tok := lexer.Read()
-// 	for tok != TokenEOF {
-// 		tokens = append(tokens, tok)
-// 		tok = lexer.Read()
-// 	}
-//
-// 	expectedLen := 8
-// 	if len(tokens) != expectedLen {
-// 		t.Errorf("returned len: %d, expected: %d", len(tokens), expectedLen)
-// 	}
-// }
-//
-
-//
-// func TestLexerKeyword(t *testing.T) {
-// 	lexer := NewLexerWithSource(`a _b ILoveGo fooBarBAZ 1_a 2b`)
-// 	for i := 0; i < 4; i++ {
-// 		tok := lexer.Peek(i)
-// 		if tok.Kind != NAME {
-// 			t.Errorf("returned kind: %d, expected: %d", tok.Kind, NAME)
-// 		}
-// 	}
-// }
-//
-// func TestParseMutation(t *testing.T) {
-// 	source := `
-// mutation {
-//   likeStory(storyID: 12345) {
-//     story {
-//       likeCount
-//     }
-//   }
-// }
-// 	`
-// 	lexer := NewLexerWithSource(source)
-// 	var tokens []Token
-//
-// 	tok := lexer.Read()
-// 	for tok != TokenEOF {
-// 		tokens = append(tokens, tok)
-// 		tok = lexer.Read()
-// 	}
-//
-// 	if !tokens[0].IsKeyword() {
-// 		t.Errorf("token %v should be a keyword", tokens[0])
-// 	}
-//
-// 	if !tokens[6].IsLiteral() {
-// 		t.Errorf("token %v should be a literal", tokens[6])
-// 	}
-//
-// 	if !tokens[1].IsPunct() {
-// 		t.Errorf("token %v should be a punctuator", tokens[1])
-// 	}
-//
-// 	expectedLen := 15
-// 	if len(tokens) != expectedLen {
-// 		t.Errorf("returned len: %d, expected: %d", len(tokens), expectedLen)
-// 	}
-//
-// 	expected := []Token{
-// 		Token{MUTATION, "mutation"},
-// 		Token{LBRACE, "{"},
-// 		Token{NAME, "likeStory"},
-// 		Token{LPAREN, "("},
-// 		Token{NAME, "storyID"},
-// 		Token{COLON, ":"},
-// 		Token{INT, "12345"},
-// 		Token{RPAREN, ")"},
-// 		Token{LBRACE, "{"},
-// 		Token{NAME, "story"},
-// 		Token{LBRACE, "{"},
-// 		Token{NAME, "likeCount"},
-// 		Token{RBRACE, "}"},
-// 		Token{RBRACE, "}"},
-// 		Token{RBRACE, "}"},
-// 	}
-//
-// 	if !reflect.DeepEqual(tokens, expected) {
-// 		t.Errorf("lexer scann error, returned: %v, expected: %v", tokens, expected)
-// 	}
-// }
-//
-// func TestParseQueryWithFragments(t *testing.T) {
-// 	source := `
-// # this is a query with nested fragments
-// query withNestedFragments {
-//   user(id: 4) {
-//     friends(first: 10) {
-//       ...friendFields
-//     }
-//     mutualFriends(first: 10) {
-//       ...friendFields
-//     }
-//   }
-// }
-//
-// # outer fragment
-// fragment friendFields on User {
-//   id
-//   name
-//   ...standardProfilePic
-// }
-//
-// # inner fragment
-// fragment standardProfilePic on User {
-//   profilePic(size: 50)
-// }
-// `
-// 	lexer := NewLexerWithSource(source)
-// 	var tokens []Token
-//
-// 	tok := lexer.Read()
-// 	for tok != TokenEOF {
-// 		tokens = append(tokens, tok)
-// 		tok = lexer.Read()
-// 	}
-//
-// 	expectedLen := 54
-// 	if len(tokens) != expectedLen {
-// 		t.Errorf("returned len: %d, expected: %d", len(tokens), expectedLen)
-// 	}
-//
-// 	expected := []Token{
-// 		Token{QUERY, "query"},
-// 		Token{NAME, "withNestedFragments"},
-// 		Token{LBRACE, "{"},
-// 		Token{NAME, "user"},
-// 		Token{LPAREN, "("},
-// 		Token{NAME, "id"},
-// 		Token{COLON, ":"},
-// 		Token{INT, "4"},
-// 		Token{RPAREN, ")"},
-// 		Token{LBRACE, "{"},
-// 		Token{NAME, "friends"},
-// 		Token{LPAREN, "("},
-// 		Token{NAME, "first"},
-// 		Token{COLON, ":"},
-// 		Token{INT, "10"},
-// 		Token{RPAREN, ")"},
-// 		Token{LBRACE, "{"},
-// 		Token{ELLIPSIS, "..."},
-// 		Token{NAME, "friendFields"},
-// 		Token{RBRACE, "}"},
-// 		Token{NAME, "mutualFriends"},
-// 		Token{LPAREN, "("},
-// 		Token{NAME, "first"},
-// 		Token{COLON, ":"},
-// 		Token{INT, "10"},
-// 		Token{RPAREN, ")"},
-// 		Token{LBRACE, "{"},
-// 		Token{ELLIPSIS, "..."},
-// 		Token{NAME, "friendFields"},
-// 		Token{RBRACE, "}"},
-// 		Token{RBRACE, "}"},
-// 		Token{RBRACE, "}"},
-// 		Token{FRAGMENT, "fragment"},
-// 		Token{NAME, "friendFields"},
-// 		Token{ON, "on"},
-// 		Token{NAME, "User"},
-// 		Token{LBRACE, "{"},
-// 		Token{NAME, "id"},
-// 		Token{NAME, "name"},
-// 		Token{ELLIPSIS, "..."},
-// 		Token{NAME, "standardProfilePic"},
-// 		Token{RBRACE, "}"},
-// 		Token{FRAGMENT, "fragment"},
-// 		Token{NAME, "standardProfilePic"},
-// 		Token{ON, "on"},
-// 		Token{NAME, "User"},
-// 		Token{LBRACE, "{"},
-// 		Token{NAME, "profilePic"},
-// 		Token{LPAREN, "("},
-// 		Token{NAME, "size"},
-// 		Token{COLON, ":"},
-// 		Token{INT, "50"},
-// 		Token{RPAREN, ")"},
-// 		Token{RBRACE, "}"},
-// 	}
-//
-// 	if !reflect.DeepEqual(tokens, expected) {
-// 		t.Errorf("lexer scann error, returned: %v, expected: %v", tokens, expected)
-// 	}
-// }
