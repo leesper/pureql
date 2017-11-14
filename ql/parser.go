5a114212299ef58c7ac9c27a549b6cc2a986657c
leesper <pascal7718@gmail.com>
2017-11-14 16:22:08 +0800

    add parser unit tests stubs

--- ql/parser.go
+++ ql/parser.go
@@ -1,25 +1,121 @@
 package ql
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
+// ErrBadToken for invalid token.
+type ErrBadToken struct {
+	line int
+	tok  Token
+}
+
+func (e ErrBadToken) Error() string {
+	return fmt.Sprintf("line %d: bad token %s", e.line, e.tok)
+}
+
+// ErrBadParse for invalid parse.
+type ErrBadParse struct {
+	line   int
+	expect string
+	found  Token
+}
+
+func (e ErrBadParse) Error() string {
+	return fmt.Sprintf("line %d: expecting %s, found %s", e.line, e.expect, e.found)
+}
+
+// Parser converts tokens into AST. This is an LL(1) grammar parser based on
+// Dr. Terence Parr's "Language Implementation Patterns".
 type Parser struct {
 	lexer     *Lexer
 	lookAhead Token
 }
 
-func NewParser(l Lexer) *Parser {
+// NewParser returns a new parser equipped with lexer.
+func NewParser(l *Lexer) *Parser {
+	return &Parser{
+		lexer:     l,
+		lookAhead: TokenEOF,
+	}
+}
+
+// Parse parses the tokens, it returns error if something goes wrong.
+func (p *Parser) Parse() error {
+	if p.lexer == nil {
+		return errors.New("lexer nil")
+	}
+	var err error
+	p.lookAhead = p.lexer.Read()
+	for p.lookAhead != TokenEOF {
+		switch p.lookAhead.Text {
+		case tokens[QUERY], tokens[MUTATION], tokens[LBRACE]:
+			err = p.document()
+			if err != nil {
+				return err
+			}
+		case tokens[INTERFACE]:
+			err = p.interfaceDefinition()
+			if err != nil {
+				return err
+			}
+		case tokens[SCALAR]:
+			err = p.scalarDefinition()
+			if err != nil {
+				return err
+			}
+		case tokens[INPUT]:
+			err = p.inputObjectDefinition()
+			if err != nil {
+				return err
+			}
+		case tokens[TYPE]:
+			err = p.typeDefinition()
+			if err != nil {
+				return err
+			}
+		case tokens[SCHEMA]:
+			err = p.schemaDefinition()
+			if err != nil {
+				return err
+			}
+		default:
+			expecting := fmt.Sprintf("%s/%s/%s/%s/%s/%s/%s/%s",
+				tokens[QUERY], tokens[MUTATION], tokens[LBRACE], tokens[INTERFACE],
+				tokens[SCALAR], tokens[INPUT], tokens[TYPE], tokens[SCHEMA])
+			return ErrBadParse{p.lexer.Line(), expecting, p.lookAhead}
+		}
+		p.lookAhead = p.lexer.Read()
+	}
 	return nil
 }
 
-func (p *Parser) Parse() error {
+func (p *Parser) interfaceDefinition() error {
 	return errors.New("not implemented")
 }
 
-func (p *Parser) Document() error {
+func (p *Parser) scalarDefinition() error {
 	return errors.New("not implemented")
 }
 
-func (p *Parser) Definition() error {
+func (p *Parser) inputObjectDefinition() error {
+	return errors.New("not implemented")
+}
+
+func (p *Parser) typeDefinition() error {
+	return errors.New("not implemented")
+}
+
+func (p *Parser) schemaDefinition() error {
+	return errors.New("not implemented")
+}
+
+func (p *Parser) document() error {
+	return errors.New("not implemented")
+}
+
+func (p *Parser) definition() error {
 	return errors.New("not implemented")
 }
 
@@ -128,26 +224,6 @@ func (p *Parser) ListType() error {
 }
 
 func (p *Parser) NonNullType() error {
-	return errors.New("not implemented")
-}
-
-func (p *Parser) InterfaceDefinition() error {
-	return errors.New("not implemented")
-}
-
-func (p *Parser) ScalarDefinition() error {
-	return errors.New("not implemented")
-}
-
-func (p *Parser) InputObjectDefinition() error {
-	return errors.New("not implemented")
-}
-
-func (p *Parser) TypeDefinition() error {
-	return errors.New("not implemented")
-}
-
-func (p *Parser) SchemaDefinition() error {
 	return errors.New("not implemented")
 }
 
