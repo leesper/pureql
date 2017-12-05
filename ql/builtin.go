b34e32b3edb4132d105bbf1a228422ae3457926d
leesper <pascal7718@gmail.com>
2017-12-05 16:45:25 +0800

    add name def to builtin scalars

--- ql/builtin.go
+++ ql/builtin.go
@@ -2,9 +2,9 @@ package ql
 
 // built-in scalar types.
 var (
-	Int     = &Scalar{}
-	Float   = &Scalar{}
-	String  = &Scalar{}
-	Boolean = &Scalar{}
-	ID      = &Scalar{}
+	Int     = &Scalar{Name: "Int"}
+	Float   = &Scalar{Name: "Float"}
+	String  = &Scalar{Name: "String"}
+	Boolean = &Scalar{Name: "Boolean"}
+	ID      = &Scalar{Name: "ID"}
 )
